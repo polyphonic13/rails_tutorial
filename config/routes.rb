f227508b52085d449f5449ef46c28486be0cc3ad
Paul Hoover <paul.hoover@selectablemedia.com>
2012-09-21 17:54:58 -0400

    beginning to work on user sign in bs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DemoApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root to: 'static_pages#home'
   
   match '/help', to: 'static_pages#help'
@@ -8,12 +9,6 @@ DemoApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # get "static_pages/home"
-  # get "static_pages/help"
-  # get "static_pages/about"
-  # get "static_pages/contact"
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
